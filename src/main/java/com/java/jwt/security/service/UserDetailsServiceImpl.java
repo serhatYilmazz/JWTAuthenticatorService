0b20ca95d468463582e4325cda4a32cc4ffaea5c
serhatYilmazz <yilmazserhat030@gmail.com>
2020-08-14 01:04:57 +0300

    Initial

--- src/main/java/com/java/jwt/security/service/UserDetailsServiceImpl.java
+++ src/main/java/com/java/jwt/security/service/UserDetailsServiceImpl.java
@@ -3,13 +3,10 @@ package com.java.jwt.security.service;
 import com.java.jwt.user.entity.UserDetailsImpl;
 import com.java.jwt.user.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
 
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
